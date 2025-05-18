de64f55ba36e8d6ed18727310c6f65f93f13e73c
HilmiSamdya <samdyahilmi@gmail.com>
2025-05-18 20:29:21 +0700

    Update Electronics.java

--- Electronics.java
+++ Electronics.java
@@ -1,10 +1,35 @@
-// Electronics.java
-public class Electronics extends Product {
+public class Electronics extends Product implements Discountable {
     private int warrantyMonths;
 
-    public Electronics(String name, double price, int productID, int warrantyMonths) {
+    // Inner class Specification
+    public class Specification {
+        private double weight;
+        private String brand;
+
+        public Specification(double weight, String brand) {
+            this.weight = weight;
+            this.brand = brand;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+
+        public String getSpecs() {
+            return "Berat: " + weight + " kg, Merek: " + brand;
+        }
+    }
+
+    private Specification specification;
+
+    public Electronics(String name, double price, int productID, int warrantyMonths, double weight, String brand) {
         super(name, price, productID);
         this.warrantyMonths = warrantyMonths;
+        this.specification = new Specification(weight, brand);
     }
 
     public int getWarrantyMonths() {
@@ -15,8 +40,22 @@ public class Electronics extends Product {
         this.warrantyMonths = warrantyMonths;
     }
 
+    public Specification getSpecification() {
+        return specification;
+    }
+
+    @Override
+    public String getCategory() {
+        return "Elektronik";
+    }
+
+    @Override
+    public double getDiscountedPrice(double discountPercentage) {
+        return getPrice() * (1 - (discountPercentage / 100));
+    }
+
     @Override
     public String getInfo() {
-        return super.getInfo() + ", Garansi: " + warrantyMonths + " bulan";
+        return super.getInfo() + ", Kategori: " + getCategory() + ", Garansi: " + warrantyMonths + " bulan, " + specification.getSpecs();
     }
 }
